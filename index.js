3fef8a464f2763c36a66bb35ae9a6064accaece9
unknown <varunpandey2604@gmail.com>
2023-02-06 12:13:37 +0530

    JS updated

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const inputs = document.querySelectorAll('.control input');
 
-function handleUpdate(){
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty('--${this.name}', this.value + suffix);
-
-}
-
-
-
 inputs.forEach(input => input.addEventListener('change', handleUpdate));
 inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+function handleUpdate(){
+    const suffix = this.dataset.sizing || '';    
+    // console.log(this.name);
+    // console.log(this.value);
+    console.log(suffix);
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
